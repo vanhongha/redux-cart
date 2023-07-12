0e4366b35b6202ef50c09ef01d779fdc8cbbbc85
vanhongha <honghaa8@gmail.com>
2023-07-12 23:10:25 +0900

    fisrt use of redux toolkit to hide cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useSelector} from "react-redux";
 
 function App() {
-  return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
-  );
+	const isCartVisible = useSelector(state => state.ui.isCartVisible);
+	return (
+		<Layout>
+			{isCartVisible && <Cart/>}
+			<Products/>
+		</Layout>
+	);
 }
 
 export default App;
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,21 @@
+import {useDispatch} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
+
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+	const dispatch = useDispatch();
+
+	const onToggleHandler = () => {
+		dispatch(uiActions.toggle());
+	}
+
+	return (
+		<button onClick={onToggleHandler} className={classes.button}>
+			<span>My Cart</span>
+			<span className={classes.badge}>1</span>
+		</button>
+	);
 };
 
 export default CartButton;
