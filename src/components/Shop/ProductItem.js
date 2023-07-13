6d0fd56465789b23cc1b032be6b394547bdd5dba
Văn Hồng Hà <honghaa8@gmail.com>
2023-07-13 04:34:51 +0000

    Update 4 files

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,24 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   const { title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const onAddItemHandler = () => {
+    dispatch(
+      cartActions.addItem({
+        item: {
+          id: "item1",
+          title: title,
+          quantity: 1,
+          price: price,
+        },
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +29,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={onAddItemHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
---
+++ src/store/cart-slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cartSlice",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem(state, action) {
+      const item = action.payload.item;
+      for (let i = 0; i < state.items.length; i++) {
+        if (item.id === state.items[i].id) {
+          state.items[i].quantity += item.quantity;
+          state.items[i].total += item.quantity * item.price;
+
+          return;
+        }
+      }
+
+      state.items.push({ ...item, total: item.quantity * item.price });
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
